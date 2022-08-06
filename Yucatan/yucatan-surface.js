f7fc8ec05f8adfcda063f19a0f2e6dd2ed38056e
Javier Alejandro Torres <87143528+javierat@users.noreply.github.com>
2022-08-06 03:07:43 -0500

    Update yucatan-surface.js

--- Yucatan/yucatan-surface.js
+++ Yucatan/yucatan-surface.js
@@ -137,9 +137,504 @@ var point22 = ee.Geometry.Point(x22,y22)
 Map.centerObject(point22, 15)
 Map.addLayer(point22, {color:'red'},'point22')
 
+var x23 = -88.376397
+var y23 = 20.257022
+var point23 = ee.Geometry.Point(x23,y23)
+Map.centerObject(point23, 15)
+Map.addLayer(point23, {color:'red'},'point23')
 
-var polygon = ee.Geometry.Polygon([[x1,y1],[x2,y2],[x3,y3],[x4,y4],[x5,y5],[x6,y6],[x7,y7],[x8,y8],[x9,y9],[x10,y10],
-                                   [x11,y11],[x12,y12],[x13,y13],[x14,y14],[x15,y15],[x16,y16],[x17,y17],[x18,y18],[x19,y19],[x20,y20],
-                                   [x21,y21],[x22,y22]])
+var x24 = -88.395409
+var y24 = 20.253557
+var point24 = ee.Geometry.Point(x24,y24)
+Map.centerObject(point24, 15)
+Map.addLayer(point24, {color:'red'},'point24')
+
+var x25 = -88.401052
+var y25 = 20.251786
+var point25 = ee.Geometry.Point(x25,y25)
+Map.centerObject(point25, 15)
+Map.addLayer(point25, {color:'red'},'point25')
+
+var x26 = -88.409099
+var y26 = 20.252088
+var point26 = ee.Geometry.Point(x26,y26)
+Map.centerObject(point26, 15)
+Map.addLayer(point26, {color:'red'},'point26')
+
+var x27 = -88.405194
+var y27 = 20.246169
+var point27 = ee.Geometry.Point(x27,y27)
+Map.centerObject(point27, 15)
+Map.addLayer(point27, {color:'red'},'point27')
+
+var x28 = -88.402898
+var y28 = 20.239485
+var point28 = ee.Geometry.Point(x28,y28)
+Map.centerObject(point28, 15)
+Map.addLayer(point28, {color:'red'},'point28')
+
+var x29 = -88.409126
+var y29 = 20.239729
+var point29 = ee.Geometry.Point(x29,y29)
+Map.centerObject(point29, 15)
+Map.addLayer(point29, {color:'red'},'point29')
+
+var x30 = -88.409126
+var y30 = 20.239729
+var point30 = ee.Geometry.Point(x30,y30)
+Map.centerObject(point30, 15)
+Map.addLayer(point30, {color:'red'},'point30')
+
+var x31 = -88.44343
+var y31 = 20.238149
+var point31 = ee.Geometry.Point(x31,y31)
+Map.centerObject(point31, 15)
+Map.addLayer(point31, {color:'red'},'point31')
+
+var x32 = -88.519945
+var y32 = 20.192354
+var point32 = ee.Geometry.Point(x32,y32)
+Map.centerObject(point32, 15)
+Map.addLayer(point32, {color:'red'},'point32')
+
+var x33 = -88.502371
+var y33 = 20.190098
+var point33 = ee.Geometry.Point(x33,y33)
+Map.centerObject(point33, 15)
+Map.addLayer(point33, {color:'red'},'point33')
+
+
+var x34 = -88.507618
+var y34 = 20.143398
+var point34 = ee.Geometry.Point(x34,y34)
+Map.centerObject(point34, 15)
+Map.addLayer(point34, {color:'red'},'point34')
+
+var x35 = -88.57668
+var y35 = 20.148085
+var point35 = ee.Geometry.Point(x35,y35)
+Map.centerObject(point35, 15)
+Map.addLayer(point35, {color:'red'},'point35')
+
+var x36 = -88.578161
+var y36 = 20.139194
+var point36 = ee.Geometry.Point(x36,y36)
+Map.centerObject(point36, 15)
+Map.addLayer(point36, {color:'red'},'point36')
+
+var x37 = -88.587666
+var y37 = 20.140463
+var point37 = ee.Geometry.Point(x37,y37)
+Map.centerObject(point37, 15)
+Map.addLayer(point37, {color:'red'},'point37')
+
+var x38 = -88.58798
+var y38 = 20.13151
+var point38 = ee.Geometry.Point(x38,y38)
+Map.centerObject(point38, 15)
+Map.addLayer(point38, {color:'red'},'point38')
+
+var x39 = -88.612701
+var y39 = 20.133127
+var point39 = ee.Geometry.Point(x39,y39)
+Map.centerObject(point39, 15)
+Map.addLayer(point39, {color:'red'},'point39')
+
+var x39 = -88.612778
+var y39 = 20.134296
+var point39 = ee.Geometry.Point(x39,y39)
+Map.centerObject(point39, 15)
+Map.addLayer(point39, {color:'red'},'point39')
+
+var x40 = -88.616388
+var y40 = 20.134462
+var point40 = ee.Geometry.Point(x40,y40)
+Map.centerObject(point40, 15)
+Map.addLayer(point40, {color:'red'},'point40')
+
+var x41 = -88.620813
+var y41 = 20.131809
+var point41 = ee.Geometry.Point(x41,y41)
+Map.centerObject(point41, 15)
+Map.addLayer(point41, {color:'red'},'point41')
+
+var x42 = -88.620941
+var y42 = 20.130439
+var point42 = ee.Geometry.Point(x42,y42)
+Map.centerObject(point42, 15)
+Map.addLayer(point42, {color:'red'},'point42')
+
+var x43 = -88.618631
+var y43 = 20.127767
+var point43 = ee.Geometry.Point(x43,y43)
+Map.centerObject(point43, 15)
+Map.addLayer(point43, {color:'red'},'point43')
+
+var x44 = -88.619174
+var y44 = 20.11717
+var point44 = ee.Geometry.Point(x44,y44)
+Map.centerObject(point44, 15)
+Map.addLayer(point44, {color:'red'},'point44')
+
+var x45 = -88.63987
+var y45 = 20.120359
+var point45 = ee.Geometry.Point(x45,y45)
+Map.centerObject(point45, 15)
+Map.addLayer(point45, {color:'red'},'point45')
+
+var x46 = -88.690613
+var y46 = 20.089868
+var point46 = ee.Geometry.Point(x46,y46)
+Map.centerObject(point46, 15)
+Map.addLayer(point46, {color:'red'},'point46')
+
+var x47 = -88.690809
+var y47 = 20.077036
+var point47 = ee.Geometry.Point(x47,y47)
+Map.centerObject(point47, 15)
+Map.addLayer(point47, {color:'red'},'point47')
+
+var x48 = -88.684892
+var y48 = 20.077424
+var point48 = ee.Geometry.Point(x48,y48)
+Map.centerObject(point48, 15)
+Map.addLayer(point48, {color:'red'},'point48')
+
+var x49 = -88.691806
+var y49 = 20.029203
+var point49 = ee.Geometry.Point(x49,y49)
+Map.centerObject(point49, 15)
+Map.addLayer(point49, {color:'red'},'point49')
+
+var x50 = -88.694757
+var y50 = 20.03004
+var point50 = ee.Geometry.Point(x50,y50)
+Map.centerObject(point50, 15)
+Map.addLayer(point50, {color:'red'},'point50')
+
+var x51 = -88.700179
+var y51 = 20.01037
+var point51 = ee.Geometry.Point(x51,y51)
+Map.centerObject(point51, 15)
+Map.addLayer(point51, {color:'red'},'point51')
+
+var x52 = -88.712538
+var y52 = 20.012306
+var point52 = ee.Geometry.Point(x52,y52)
+Map.centerObject(point52, 15)
+Map.addLayer(point52, {color:'red'},'point52')
+
+var x53 = -88.713053
+var y53 = 20.00917
+var point53 = ee.Geometry.Point(x53,y53)
+Map.centerObject(point53, 15)
+Map.addLayer(point53, {color:'red'},'point53')
+
+var x54 = -88.718439
+var y54 = 20.01033
+var point54 = ee.Geometry.Point(x54,y54)
+Map.centerObject(point54, 15)
+Map.addLayer(point54, {color:'red'},'point54')
+
+var x55 = -88.722151
+var y55 = 20.005125
+var point55 = ee.Geometry.Point(x55,y55)
+Map.centerObject(point55, 15)
+Map.addLayer(point55, {color:'red'},'point55')
+
+var x56 = -88.723771
+var y56 = 20.005508
+var point56 = ee.Geometry.Point(x56,y56)
+Map.centerObject(point56, 15)
+Map.addLayer(point56, {color:'red'},'point56')
+
+var x57 = -88.722784
+var y57 = 20.013206
+var point57 = ee.Geometry.Point(x57,y57)
+Map.centerObject(point57, 15)
+Map.addLayer(point57, {color:'red'},'point57')
+
+var x58 = -88.735981
+var y58 = 20.017601
+var point58 = ee.Geometry.Point(x58,y58)
+Map.centerObject(point58, 15)
+Map.addLayer(point58, {color:'red'},'point58')
+
+var x59 = -88.73582
+var y59 = 20.015998
+var point59 = ee.Geometry.Point(x59,y59)
+Map.centerObject(point59, 15)
+Map.addLayer(point59, {color:'red'},'point59')
+
+var x60 = -88.734146
+var y60 = 20.015272
+var point60 = ee.Geometry.Point(x60,y60)
+Map.centerObject(point60, 15)
+Map.addLayer(point60, {color:'red'},'point60')
+
+var x61 = -88.733191
+var y61 = 20.014435
+var point61 = ee.Geometry.Point(x61,y61)
+Map.centerObject(point61, 15)
+Map.addLayer(point61, {color:'red'},'point61')
+
+var x62 = -88.732365
+var y62 = 20.014083
+var point62 = ee.Geometry.Point(x62,y62)
+Map.centerObject(point62, 15)
+Map.addLayer(point62, {color:'red'},'point62')
+
+var x63 = -88.736217
+var y63 = 20.009677
+var point63 = ee.Geometry.Point(x63,y63)
+Map.centerObject(point63, 15)
+Map.addLayer(point63, {color:'red'},'point63')
+
+var x64 = -88.742623
+var y64 = 20.008575
+var point64 = ee.Geometry.Point(x64,y64)
+Map.centerObject(point64, 15)
+Map.addLayer(point64, {color:'red'},'point64')
+
+var x65 = -88.742473
+var y65 = 20.015101
+var point65 = ee.Geometry.Point(x65,y65)
+Map.centerObject(point65, 15)
+Map.addLayer(point65, {color:'red'},'point65')
+
+var x66 = -88.757772
+var y66 = 20.013952
+var point66 = ee.Geometry.Point(x66,y66)
+Map.centerObject(point66, 15)
+Map.addLayer(point66, {color:'red'},'point66')
+
+var x67 = -88.757697
+var y67 = 20.012198
+var point67 = ee.Geometry.Point(x67,y67)
+Map.centerObject(point67, 15)
+Map.addLayer(point67, {color:'red'},'point67')
+
+var x68 = -88.771451
+var y68 = 20.013458
+var point68 = ee.Geometry.Point(x68,y68)
+Map.centerObject(point68, 15)
+Map.addLayer(point68, {color:'red'},'point68')
+
+var x69 = -88.771194
+var y69 = 20.007682
+var point69 = ee.Geometry.Point(x69,y69)
+Map.centerObject(point69, 15)
+Map.addLayer(point69, {color:'red'},'point69')
+
+var x70 = -88.78351
+var y70 = 20.008186
+var point70 = ee.Geometry.Point(x70,y70)
+Map.centerObject(point70, 15)
+Map.addLayer(point70, {color:'red'},'point70')
+
+var x71 = -88.784431
+var y71 = 19.995238
+var point71 = ee.Geometry.Point(x71,y71)
+Map.centerObject(point71, 15)
+Map.addLayer(point71, {color:'red'},'point71')
+
+var x72 = -88.789195
+var y72 = 19.995561
+var point72 = ee.Geometry.Point(x72,y72)
+Map.centerObject(point72, 15)
+Map.addLayer(point72, {color:'red'},'point72')
+
+var x73 = -88.790482
+var y73 = 19.992173
+var point73 = ee.Geometry.Point(x73,y73)
+Map.centerObject(point73, 15)
+Map.addLayer(point73, {color:'red'},'point73')
+
+var x74 = -88.790761
+var y74 = 19.971997
+var point74 = ee.Geometry.Point(x74,y74)
+Map.centerObject(point74, 15)
+Map.addLayer(point74, {color:'red'},'point74')
+
+var x75 = -88.798017
+var y75 = 19.974929
+var point75 = ee.Geometry.Point(x75,y75)
+Map.centerObject(point75, 15)
+Map.addLayer(point75, {color:'red'},'point75')
+
+var x76 = -88.798778
+var y76 = 19.970512
+var point76 = ee.Geometry.Point(x76,y76)
+Map.centerObject(point76, 15)
+Map.addLayer(point76, {color:'red'},'point76')
+
+var x77 = -88.798392
+var y77 = 19.952529
+var point77 = ee.Geometry.Point(x77,y77)
+Map.centerObject(point77, 15)
+Map.addLayer(point77, {color:'red'},'point77')
+
+var x78 = -88.805623
+var y78 = 19.953832
+var point78 = ee.Geometry.Point(x78,y78)
+Map.centerObject(point78, 15)
+Map.addLayer(point78, {color:'red'},'point78')
+
+var x79 = -88.806052
+var y79 = 19.944583
+var point79 = ee.Geometry.Point(x79,y79)
+Map.centerObject(point79, 15)
+Map.addLayer(point79, {color:'red'},'point79')
+
+var x80 = -88.810162
+var y80 = 19.944956
+var point80 = ee.Geometry.Point(x80,y80)
+Map.centerObject(point80, 15)
+Map.addLayer(point80, {color:'red'},'point80')
+
+var x81 = -88.811441
+var y81 = 19.935238
+var point81 = ee.Geometry.Point(x81,y81)
+Map.centerObject(point81, 15)
+Map.addLayer(point81, {color:'red'},'point81')
+
+var x82 = -88.808909
+var y82 = 19.934129
+var point82 = ee.Geometry.Point(x82,y82)
+Map.centerObject(point82, 15)
+Map.addLayer(point82, {color:'red'},'point82')
+
+var x82 = -88.808909
+var y82 = 19.934129
+var point82 = ee.Geometry.Point(x82,y82)
+Map.centerObject(point82, 15)
+Map.addLayer(point82, {color:'red'},'point82')
+
+var x83 = -88.809853
+var y83 = 19.926996
+var point83 = ee.Geometry.Point(x83,y83)
+Map.centerObject(point83, 15)
+Map.addLayer(point83, {color:'red'},'point83')
+
+var x84 = -88.809741
+var y84 = 19.925999
+var point84 = ee.Geometry.Point(x84,y84)
+Map.centerObject(point84, 15)
+Map.addLayer(point84, {color:'red'},'point84')
+
+var x85 = -88.812589
+var y85 = 19.925555
+var point85 = ee.Geometry.Point(x85,y85)
+Map.centerObject(point85, 15)
+Map.addLayer(point85, {color:'red'},'point85')
+
+var x86 = -88.813334
+var y86 = 19.918287
+var point86 = ee.Geometry.Point(x86,y86)
+Map.centerObject(point86, 15)
+Map.addLayer(point86, {color:'red'},'point86')
+
+var x86 = -88.816145
+var y86 = 19.915503
+var point86 = ee.Geometry.Point(x86,y86)
+Map.centerObject(point86, 15)
+Map.addLayer(point86, {color:'red'},'point86')
+
+var x87 = -88.816508
+var y87 = 19.904479
+var point87 = ee.Geometry.Point(x87,y87)
+Map.centerObject(point87, 15)
+Map.addLayer(point87, {color:'red'},'point87')
+
+var x88 = -88.817162
+var y88 = 19.904943
+var point88 = ee.Geometry.Point(x88,y88)
+Map.centerObject(point88, 15)
+Map.addLayer(point88, {color:'red'},'point88')
+
+var x89 = -88.817881
+var y89 = 19.904065
+var point89 = ee.Geometry.Point(x89,y89)
+Map.centerObject(point89, 15)
+Map.addLayer(point89, {color:'red'},'point89')
+
+var x90 = -88.820177
+var y90 = 19.905553
+var point90 = ee.Geometry.Point(x90,y90)
+Map.centerObject(point90, 15)
+Map.addLayer(point90, {color:'red'},'point90')
+
+var x91 = -88.822746
+var y91 = 19.904156
+var point91 = ee.Geometry.Point(x91,y91)
+Map.centerObject(point91, 15)
+Map.addLayer(point91, {color:'red'},'point91')
+
+var x92 = -88.824431
+var y92 = 19.904764
+var point92 = ee.Geometry.Point(x92,y92)
+Map.centerObject(point92, 15)
+Map.addLayer(point92, {color:'red'},'point92')
+
+var x93 = -88.823154
+var y93 = 19.908157
+var point93 = ee.Geometry.Point(x93,y93)
+Map.centerObject(point93, 15)
+Map.addLayer(point93, {color:'red'},'point93')
+
+var x94 = -88.827284
+var y94 = 19.91092
+var point94 = ee.Geometry.Point(x94,y94)
+Map.centerObject(point94, 15)
+Map.addLayer(point94, {color:'red'},'point94')
+
+var x95 = -88.82751
+var y95 = 19.910615
+var point95 = ee.Geometry.Point(x95,y95)
+Map.centerObject(point95, 15)
+Map.addLayer(point95, {color:'red'},'point95')
+
+var x96 = -88.827282
+var y96 = 19.910479
+var point96 = ee.Geometry.Point(x96,y96)
+Map.centerObject(point96, 15)
+Map.addLayer(point96, {color:'red'},'point96')
+
+var x97 = -88.829436
+var y97 = 19.906016
+var point97 = ee.Geometry.Point(x97,y97)
+Map.centerObject(point97, 15)
+Map.addLayer(point97, {color:'red'},'point97')
+
+var x98 = -88.827371
+var y98 = 19.904489
+var point98 = ee.Geometry.Point(x98,y98)
+Map.centerObject(point98, 15)
+Map.addLayer(point98, {color:'red'},'point98')
+
+var x99 = -88.829824
+var y99 = 19.898558
+var point99 = ee.Geometry.Point(x99,y99)
+Map.centerObject(point99, 15)
+Map.addLayer(point99, {color:'red'},'point99')
+
+var x99 = -88.829824
+var y99 = 19.898558
+var point99 = ee.Geometry.Point(x99,y99)
+Map.centerObject(point99, 15)
+Map.addLayer(point99, {color:'red'},'point99')
+
+var x100 = -88.829824
+var y100 = 19.898558
+var point100 = ee.Geometry.Point(x100,y100)
+Map.centerObject(point100, 15)
+Map.addLayer(point100, {color:'red'},'point100')
+
+var polygon = ee.Geometry.Polygon([[x1,y1],[x2,y2],[x3,y3],[x4,y4],[x5,y5],[x6,y6],[x7,y7],[x8,y8],[x9,y9],[x10,y10],[x11,y11],[x12,y12],[x13,y13],[x14,y14],[x15,y15],[x16,y16],[x17,y17],[x18,y18],[x19,y19],[x20,y20],
+                                  [x21,y21],[x22,y22],[x23,y23],[x24,y24],[x25,y25],[x26,y26],[x27,y27],[x28,y28],[x29,y29],[x30,y30],[x31,y31],[x32,y32],[x33,y33],[x34,y34],[x35,y35],[x36,y36],[x37,y37],[x38,y39],
+                                  [x40,y40],[x41,y41],[x42,y42],[x43,y43],[x44,y44],[x45,y45],[x46,y46],[x47,y47],[x48,y48],[x49,y49],[x50,y50],[x51,y51],[x52,y52],[x53,y53],[x54,y54],[x55,y55],[x56,y56],[x57,y57],[x58,y58],
+                                  [x59,y59],[x60,y60],[x61,y61],[x62,y62],[x63,y63],[x64,y64],[x65,y65],[x66,y66],[x67,y67],[x68,y68],[x69,y69],[x70,y70],[x71,y71],[x72,y72],[x73,y73],[x74,y74],[x75,y75],[x76,y76],[x77,y77],
+                                  [x78,y78],[x79,y79],[x80,y80],[x81,y81],[x82,y82],[x83,y83],[x84,y84],[x85,y85],[x86,y86],[x87,y87],[x88,y88],[x89,y89],[x90,y90],[x91,y91],[x92,y92],[x93,y93],[x94,y94],[x95,y95],[x96,y96],
+                                  [x97,y97],[x98,y98],[x99,y99],[x100,y100]])
 
 Map.addLayer(polygon,{color:'blue'},'polygon')
